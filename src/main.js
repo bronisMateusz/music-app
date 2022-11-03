49dcd347a456e45cb3c3f944c13897c670fe2b92
bronisMateusz <mateusz.bronis99@gmail.com>
2022-11-03 21:57:41 +0100

    Reorganize styles

--- src/main.js
+++ src/main.js
@@ -4,8 +4,7 @@ import { createPinia } from "pinia";
 import App from "./App.vue";
 import router from "./router";
 
-import "./assets/base.scss";
-import "./assets/main.scss";
+import "./assets/scss/styles.scss";
 
 const app = createApp(App);
 
