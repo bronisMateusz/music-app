81b3e442d2e0a23a9de0c578dfa59a6a36b9b951
bronisMateusz <mateusz.bronis@smartbees.co>
2022-11-13 01:52:17 +0100

    Add Footer component with basic styles

---
+++ src/components/Footer.vue
@@ -0,0 +1,12 @@
+<template>
+  <footer id="footer">
+    <ul>
+      <li>
+        <a href="#">Cookies</a>
+      </li>
+      <li>
+        <a href="#">Privacy</a>
+      </li>
+    </ul>
+  </footer>
+</template>
