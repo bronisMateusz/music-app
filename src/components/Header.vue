402c531bdc2f4ffc8c0a57cc853bd375f5b359b7
bronisMateusz <mateusz.bronis99@gmail.com>
2022-11-13 01:51:54 +0100

    Add Header component with basic styles

---
+++ src/components/Header.vue
@@ -0,0 +1,21 @@
+<template>
+  <header id="header">
+    <nav>
+      <!-- Page Name -->
+      <h1>Home</h1>
+
+      <div>
+        <!-- Primary Navigation -->
+        <ul>
+          <!-- Navigation Links -->
+          <li>
+            <a href="#">
+              Login / Register
+              <eva-icon name="person-outline"></eva-icon>
+            </a>
+          </li>
+        </ul>
+      </div>
+    </nav>
+  </header>
+</template>
