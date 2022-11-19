f8909ab719eef051fa9aeea67e610f6e2eab3aa0
bronisMateusz <mateusz.bronis@smartbees.co>
2022-11-19 23:15:07 +0100

    Add firebase config script

---
+++ src/includes/firebase.js
@@ -0,0 +1,12 @@
+import firebase from "firebase/compat/app";
+import "firebase/auth";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDXBZJoWyFfNBm7Xfz15YBgmF5A3jyB6eE",
+  authDomain: "music-app-11ce6.firebaseapp.com",
+  projectId: "music-app-11ce6",
+  storageBucket: "music-app-11ce6.appspot.com",
+  appId: "1:754991169134:web:b8fde588bcbeec8f1df07f",
+};
+
+export default firebase.initializeApp(firebaseConfig);
