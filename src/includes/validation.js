778fe72484e6f31eaa3013e0f474c34015661564
bronisMateusz <mateusz.bronis99@gmail.com>
2022-11-19 16:00:47 +0100

    Add custom error messages

--- src/includes/validation.js
+++ src/includes/validation.js
@@ -3,6 +3,7 @@ import {
   Field as VeeField,
   defineRule,
   ErrorMessage,
+  configure,
 } from "vee-validate";
 import {
   required,
@@ -20,10 +21,31 @@ export default {
     app.component("ErrorMessage", ErrorMessage);
 
     defineRule("required", required);
+    defineRule("tos", required);
     defineRule("min", min);
     defineRule("max", max);
     defineRule("alphaSpaces", alpha_spaces);
     defineRule("email", email);
-    defineRule("confirmed", confirmed);
+    defineRule("passwordMismatch", confirmed);
+
+    configure({
+      generateMessage: (ctx) => {
+        const messages = {
+          required: `Enter an ${ctx.field}`,
+          min: `${ctx.field} is to short`,
+          max: `${ctx.field} is to long`,
+          alpha_spaces: `${ctx.field} may only contain alphabetical characters and spaces`,
+          email: `${ctx.field} should be a valid email`,
+          passwordMismatch: "Passwords are different",
+          tos: "Accept the Terms of Service",
+        };
+
+        const message = messages[ctx.rule.name]
+          ? messages[ctx.rule.name]
+          : `${ctx.field} isn't valid`;
+
+        return message;
+      },
+    });
   },
 };
