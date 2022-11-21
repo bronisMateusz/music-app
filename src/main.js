d521bf6bc4331788389798b6439620588c83789f
bronisMateusz <mateusz.bronis@smartbees.co>
2022-11-21 23:16:34 +0100

    Set firebase initiation before vue

--- src/main.js
+++ src/main.js
@@ -5,15 +5,20 @@ import * as evaIcons from "@stefandesu/eva-icons-vue";
 import App from "./App.vue";
 import router from "./router";
 import VeeValidate from "./includes/validation";
-import "./includes/firebase";
+import { auth } from "./includes/firebase";
 
 import "./assets/scss/styles.scss";
 
-const app = createApp(App);
+let app;
+auth.onAuthStateChanged(() => {
+  if (!app) {
+    app = createApp(App);
 
-app.use(createPinia());
-app.use(router);
-app.use(evaIcons);
-app.use(VeeValidate);
+    app.use(createPinia());
+    app.use(router);
+    app.use(evaIcons);
+    app.use(VeeValidate);
 
-app.mount("#app");
+    app.mount("#app");
+  }
+});
