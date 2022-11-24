4702a46142919e45ed73b79773ae7598588d7276
bronisMateusz <mateusz.bronis99@gmail.com>
2022-11-24 18:46:59 +0100

    Add styles to Notification component

--- src/components/Notification.vue
+++ src/components/Notification.vue
@@ -7,3 +7,54 @@
     </div>
   </div>
 </template>
+
+<styles lang="scss">
+@import "@/assets/scss/abstracts/variables";
+
+#notification {
+  align-items: flex-start;
+  backdrop-filter: blur(15px);
+  background-color: rgba($color: $color-element, $alpha: 0.5);
+  border-radius: 50px;
+  box-shadow: 0px 8px 24px rgba(0, 0, 0, 0.25);
+  column-gap: 10px;
+  display: flex;
+  left: 50%;
+  padding: 5px 24px 5px 5px;
+  position: fixed;
+  top: 15px;
+  transform: translateX(-50%);
+
+  &.notice {
+    color: $text-notice;
+  }
+
+  &.success {
+    color: $text-success;
+  }
+
+  &.error {
+    color: $text-error;
+  }
+
+  svg {
+    top: 0 !important;
+  }
+
+  .notification-details {
+    display: flex;
+    flex-direction: column;
+    margin-top: 6px;
+    row-gap: 2px;
+
+    strong {
+      font-size: 18px;
+      font-weight: 700;
+    }
+
+    p {
+      color: $text-primary;
+    }
+  }
+}
+</styles>
