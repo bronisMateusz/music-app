11f54b87562f76115021e6f9fd0636a26cf37bbe
bronisMateusz <mateusz.bronis99@gmail.com>
2022-11-24 21:32:51 +0100

    Remove getters from modal store

--- src/stores/modal.js
+++ src/stores/modal.js
@@ -4,9 +4,4 @@ export default defineStore("modal", {
   state: () => ({
     isOpen: false,
   }),
-  getters: {
-    hiddenClass(state) {
-      return !state.isOpen ? "hidden" : "";
-    },
-  },
 });
