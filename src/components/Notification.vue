3aa52b87d61ae3cd1585147f231ef8e5a1ca9563
bronisMateusz <mateusz.bronis99@gmail.com>
2022-11-24 22:27:03 +0100

    Add notification wrapper and update styles in notification component

--- src/components/Notification.vue
+++ src/components/Notification.vue
@@ -1,31 +1,33 @@
 <template>
-  <div
-    v-show="showNotification !== false"
-    id="notification"
-    :class="notificationType"
-  >
-    <eva-icon
-      v-if="notificationType === 'success'"
-      name="checkmark-circle-2"
-      height="48"
-      width="48"
-    />
-    <eva-icon
-      v-else-if="notificationType === 'error'"
-      name="close-circle"
-      height="48"
-      width="48"
-    />
-    <eva-icon v-else name="alert-circle" height="48" width="48" />
-    <div class="notification-details">
-      <strong>{{ notificationHeading }}</strong>
-      <p>{{ notificationMsg }}</p>
+  <div id="notification-wrapper">
+    <div
+      v-if="!!showNotification"
+      class="notification"
+      :class="notificationType"
+    >
+      <eva-icon
+        v-if="notificationType === 'success'"
+        name="checkmark-circle-2"
+        height="48"
+        width="48"
+      />
+      <eva-icon
+        v-else-if="notificationType === 'error'"
+        name="close-circle"
+        height="48"
+        width="48"
+      />
+      <eva-icon v-else name="alert-circle" height="48" width="48" />
+      <div class="notification-details">
+        <strong>{{ notificationHeading }}</strong>
+        <p>{{ notificationMsg }}</p>
+      </div>
     </div>
   </div>
 </template>
 
 <script>
-import { mapWritableState } from "pinia";
+import { mapWritableState, mapActions } from "pinia";
 import useNotificationsStore from "@/stores/notifications";
 
 export default {
@@ -37,64 +39,72 @@ export default {
       "notificationMsg",
     ]),
   },
+  methods: {
+    ...mapActions(useNotificationsStore, ["autoHideNotification"]),
+  },
+  updated() {
+    this.autoHideNotification();
+  },
 };
 </script>
 
 <styles lang="scss">
-#notification {
-  align-items: flex-start;
-  backdrop-filter: blur(15px);
-  background-color: rgba($color: $color-element, $alpha: 0.5);
+#notification-wrapper {
   border-radius: 30px;
-  box-shadow: 0px 8px 24px rgba(0, 0, 0, 0.25);
-  column-gap: 10px;
   display: flex;
-  left: 50%;
-  max-width: calc(100% - 48px);
-  padding: 5px 24px 5px 5px;
+  justify-content: center;
+  left: 24px;
+  margin: 0 auto;
+  max-width: 400px;
   position: fixed;
+  right: 24px;
   top: 15px;
-  transform: translateX(-50%);
-  width: 100%;
-
-  &.notice {
-    color: $text-notice;
-  }
-
-  &.success {
-    color: $text-success;
-  }
-
-  &.error {
-    color: $text-error;
-  }
-
-  svg {
-    min-width: 48px;
-    top: 0 !important;
-  }
-
-  .notification-details {
+  z-index: 2;
+  transition: all 1s ease-in-out;
+  .notification {
+    align-items: flex-start;
+    backdrop-filter: blur(15px);
+    background-color: rgba($color: $color-element, $alpha: 0.5);
+    border-radius: 30px;
+    box-shadow: 0px 8px 24px rgba(0, 0, 0, 0.25);
+    column-gap: 10px;
     display: flex;
-    flex-direction: column;
-    margin: 3.5px 0;
-    row-gap: 2px;
-
-    strong {
-      font-size: 18px;
-      font-weight: 700;
-    }
-
-    p {
-      color: $text-primary;
-      line-height: 1.25rem;
-    }
-  }
-}
-
-@media (min-width: 768px) {
-  #notification {
     max-width: 400px;
+    padding: 5px 24px 5px 5px;
+
+    &.notice {
+      color: $text-notice;
+    }
+
+    &.success {
+      color: $text-success;
+    }
+
+    &.error {
+      color: $text-error;
+    }
+
+    svg {
+      min-width: 48px;
+      top: 0 !important;
+    }
+
+    .notification-details {
+      display: flex;
+      flex-direction: column;
+      margin: 3.5px 0;
+      row-gap: 2px;
+
+      strong {
+        font-size: 18px;
+        font-weight: 700;
+      }
+
+      p {
+        color: $text-primary;
+        line-height: 1.25rem;
+      }
+    }
   }
 }
 </styles>
