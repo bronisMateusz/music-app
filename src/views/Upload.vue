981e8cfeb854ab945392c6dd43a29d650fe1d4ed
bronisMateusz <mateusz.bronis99@gmail.com>
2022-12-17 16:57:54 +0100

    Add drop-zone element to Upload view

--- src/views/Upload.vue
+++ src/views/Upload.vue
@@ -2,6 +2,24 @@
   <!-- Upload -->
   <section>
     <h2>Upload</h2>
+    <div
+      id="drop-zone"
+      @dragend.prevent.stop="is_dragover = false"
+      @dragover.prevent.stop="is_dragover = true"
+      @dragenter.prevent.stop="is_dragover = true"
+      @dragleave.prevent.stop="is_dragover = false"
+      @drop.prevent.stop="upload($event)"
+    >
+      <div v-if="!is_dragover">
+        <eva-icon name="cloud-upload-outline" height="72" width="72" />
+        <p>Drop your file(s) here or browse</p>
+        <span>Maximum file size is 25 MB</span>
+      </div>
+      <div v-else>
+        <eva-icon name="cloud-upload-outline" height="72" width="72" />
+        <p>Drop your file(s) to upload</p>
+      </div>
+    </div>
     <input type="file" />
   </section>
   <!-- Upload progress -->
@@ -45,3 +63,48 @@
     </ul>
   </section>
 </template>
+<script>
+export default {
+  data() {
+    return {
+      is_dragover: false,
+    };
+  },
+  methods: {
+    upload($event) {
+      this.is_dragover = false;
+      console.log($event)
+    },
+  },
+};
+</script>
+<style lang="scss">
+#drop-zone {
+  @include blurred-bg;
+  background-image: url("data:image/svg+xml,%3csvg width='100%25' height='100%25' xmlns='http://www.w3.org/2000/svg'%3e%3crect width='100%25' height='100%25' fill='none' rx='20' ry='20' stroke='%2356504D' stroke-width='3' stroke-dasharray='12' stroke-dashoffset='0' stroke-linecap='round'/%3e%3c/svg%3e");
+  border-radius: 20px;
+  height: 254px;
+
+  div {
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    height: 100%;
+
+    svg,
+    span {
+      color: $text-secondary;
+    }
+
+    svg {
+      margin-bottom: 36px;
+    }
+
+    p {
+      font-size: 1.375rem;
+      margin-bottom: 12px;
+    }
+  }
+}
+</style>
