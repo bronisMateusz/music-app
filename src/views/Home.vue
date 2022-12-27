1dc8b9643bd9919f6c95ebd70094da8dff383b98
bronisMateusz <mateusz.bronis99@gmail.com>
2022-12-27 16:22:35 +0100

    Add latest-albums section

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,5 +1,46 @@
 <template>
   <section>
+    <ul id="latest-albums">
+      <li>
+        <article class="album">
+          <aurora-gradient />
+          <section>
+            <h2>₪₪₪ MadeHewraGreatForever ₪₪₪</h2>
+            <p>New Album by <strong>HEWRA</strong></p>
+          </section>
+        </article>
+      </li>
+      <li>
+        <article class="album">
+          <aurora-gradient
+            colorFirst="#FF6A37"
+            colorSecond="#FFD31F"
+            colorThird="#FF3969"
+            colorFourth="#FF2528"
+          />
+          <section>
+            <h2>Cock.0z Mixtape</h2>
+            <p>New Album by <strong>Kaz Bałagane</strong></p>
+          </section>
+        </article>
+      </li>
+      <li>
+        <article class="album">
+          <aurora-gradient
+            colorFirst="#BBC0FF"
+            colorSecond="#00DDFF"
+            colorThird="#79E9FF"
+            colorFourth="#327DF6"
+          />
+          <section>
+            <h2>Mobbyn</h2>
+            <p>New Album by <strong>Mobbyn</strong></p>
+          </section>
+        </article>
+      </li>
+    </ul>
+  </section>
+  <section>
     <h2>Songs</h2>
     <!-- Playlist -->
     <ul id="playlist">
@@ -73,7 +114,12 @@
   </section>
 </template>
 
-<script></script>
+<script>
+import AuroraGradient from "@/components/AuroraGradient.vue";
+export default {
+  components: { AuroraGradient },
+};
+</script>
 
 <style lang="scss">
 * {
@@ -117,6 +163,42 @@ body {
     min-height: 100vh;
     display: flex;
     flex-direction: column;
+
+    #latest-albums {
+      overflow-x: hidden;
+      display: flex;
+      gap: 24px;
+      list-style: none;
+      padding: 0;
+
+      .album {
+        height: 186px;
+        width: 275px;
+        position: relative;
+
+        section {
+          border-radius: 20px;
+          color: $text-primary-inverted;
+          display: flex;
+          flex-direction: column-reverse;
+          gap: 12px;
+          inset: 0;
+          padding: 12px;
+          position: absolute;
+
+          h2 {
+            font-size: 22px;
+            line-height: 1.5rem;
+            margin: 0;
+            overflow-wrap: break-word;
+          }
+
+          p {
+            font-size: 0.75rem;
+          }
+        }
+      }
+    }
 
     #playlist {
       @include songs-list;
