a29ff144686c9f541b0657ea1901c2ed950c64aa
bronisMateusz <mateusz.bronis99@gmail.com>
2022-12-29 00:07:47 +0100

    Add ability to play audio

--- src/stores/player.js
+++ src/stores/player.js
@@ -1,12 +1,20 @@
 import { defineStore } from "pinia";
+import { Howl } from "howler";
 
 export default defineStore("player", {
   state: () => ({
     currentSong: {},
+    sound: {},
   }),
   actions: {
     async newSong(song) {
       this.currentSong = song;
+
+      this.sound = new Howl({
+        src: [song.url],
+        html5: true,
+      });
+      this.sound.play();
     },
   },
 });
