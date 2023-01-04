ad53b05b421576badb18d0ac10df6003c78c9287
bronisMateusz <mateusz.bronis99@gmail.com>
2023-01-04 20:37:42 +0100

    Move user menu element to separate component

--- src/components/AuthButton.vue
+++ src/components/AuthButton.vue
@@ -2,44 +2,39 @@
   <div class="auth-btn-wrapper">
     <button
       class="auth-btn"
-      v-if="!userLoggedIn"
-      title="Login / Register"
-      @click.prevent="toggleAuthModal"
+      :title="!userLoggedIn ? 'Login / Register' : 'User menu'"
+      @click.prevent="!userLoggedIn ? toggleAuthModal() : toggleUserMenu()"
     >
       <eva-icon name="people-outline" height="28" width="28" />
     </button>
-    <button v-else class="auth-btn">
-      <eva-icon name="people-outline" height="28" width="28" />
-    </button>
-    <div class="auth-dropdown">
-      <ul>
-        <li>
-          <router-link :to="{ name: 'user', params: { id: '1' } }">
-            Settings
-          </router-link>
-        </li>
-        <li>
-          <button title="Logout" @click.prevent="logout">Logout</button>
-        </li>
-      </ul>
-    </div>
+    <user-menu v-if="isUserMenuOpen" @closeMenu="isUserMenuOpen = false" />
   </div>
 </template>
 
 <script>
-import { mapActions, mapWritableState } from "pinia";
+import { mapState, mapWritableState } from "pinia";
 import useAuthModalStore from "@/stores/auth-modal";
 import useUserStore from "@/stores/user";
+import UserMenu from "@/components/UserMenu.vue";
 
 export default {
+  data() {
+    return {
+      isUserMenuOpen: false,
+    };
+  },
+  components: { UserMenu },
   computed: {
     ...mapWritableState(useAuthModalStore, ["isOpen"]),
-    ...mapWritableState(useUserStore, ["userLoggedIn"]),
+    ...mapState(useUserStore, ["userLoggedIn"]),
   },
   methods: {
-    ...mapActions(useUserStore, ["logout"]),
     toggleAuthModal() {
       this.isOpen = !this.isOpen;
+    },
+
+    toggleUserMenu() {
+      this.isUserMenuOpen = !this.isUserMenuOpen;
     },
   },
 };
@@ -63,59 +58,9 @@ export default {
       background-color: $color-canvas;
       color: $text-primary;
     }
-  }
 
-  .auth-dropdown {
-    position: absolute;
-    right: 0;
-    top: calc(100%);
-
-    &::after {
-      @include blurred-bg;
-      content: "";
-      border-radius: 8px;
-      inset: 0;
-      position: absolute;
-      z-index: -1;
-    }
-
-    ul {
-      @include hidden-list-marks;
-      text-align: right;
-
-      a,
-      button {
-        padding: 12px 24px 12px 48px;
-      }
-
-      a {
-        display: block;
-        border-bottom: 1px solid $color-border-primary;
-        text-decoration: none;
-
-        &:not(:hover) {
-          color: $text-secondary;
-        }
-      }
-      button {
-        font-size: 1rem;
-      }
-    }
-
-    @media (min-width: 992px) {
-      left: 0;
-      right: unset;
-      top: calc(100% + 12px);
-
-      ul {
-        text-align: left;
-
-        a,
-        button {
-          padding-left: 24px;
-          padding-right: 48px;
-        }
-      }
+    svg {
+      pointer-events: none;
     }
   }
 }
