c37651a93bd68e1f224f10be0b6e502bdc5a7780
bronisMateusz <mateusz.bronis99@gmail.com>
2023-01-06 20:13:09 +0100

    Add setStoreDetails and updateProfile actions to user store

--- src/stores/user.js
+++ src/stores/user.js
@@ -9,32 +9,36 @@ import {
   signOut,
   updateProfile,
 } from "firebase/auth";
-import { setDoc, doc } from "firebase/firestore";
+import { doc, updateDoc, setDoc } from "firebase/firestore";
 
 export default defineStore("user", {
   state: () => ({
+    displayName: "",
     userLoggedIn: false,
     userId: "",
   }),
   actions: {
     async register(values) {
+      // Create user
       const userCred = await createUserWithEmailAndPassword(
         auth,
         values.email,
         values.password
       );
 
-      await setDoc(doc(db, "users", userCred.user.uid), {
-        name: values.name,
-        email: values.email,
-      });
-
+      // Set user profile in firebase
       await updateProfile(userCred.user, {
         displayName: values.name,
       });
 
-      this.userLoggedIn = true;
-      this.userId = userCred.user.uid;
+      // Set user data in firestore
+      await setDoc(doc(db, "users", userCred.user.uid), {
+        displayName: values.name,
+        email: values.email,
+      });
+
+      // Set store details
+      this.setStoreDetails(userCred.user);
     },
 
     async login(values) {
@@ -55,8 +59,8 @@ export default defineStore("user", {
             })
         : await signInWithEmailAndPassword(auth, values.email, values.password);
 
-      this.userLoggedIn = true;
-      this.userId = auth.currentUser.uid;
+      // Set store details
+      this.setStoreDetails(auth.currentUser);
     },
 
     async logout() {
@@ -66,6 +70,24 @@ export default defineStore("user", {
 
     async resetPassword(email) {
       await sendPasswordResetEmail(auth, email);
+    },
+
+    setStoreDetails(user) {
+      this.userLoggedIn = true;
+      this.displayName = user.displayName;
+      this.userId = user.uid;
+    },
+
+    async updateProfile(user) {
+      // Update user profile in firebase
+      await updateProfile(auth.currentUser, user);
+
+      // Update user data in firestore
+      const userRef = doc(db, "users", this.userId);
+      await updateDoc(userRef, user);
+
+      // Update store displayName
+      this.displayName = user.displayName;
     },
   },
 });
