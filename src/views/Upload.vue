3d44110e128abdb7227c5acf7bfc38f18926a654
bronisMateusz <mateusz.bronis99@gmail.com>
2023-01-21 12:59:31 +0100

    Refactor Upload view

--- src/views/Upload.vue
+++ src/views/Upload.vue
@@ -124,11 +124,12 @@ import {
   arrayUnion,
   collection,
   doc,
-  getDocs,
-  query,
-  where,
   getDoc,
+  getDocs,
+  limit,
+  query,
   updateDoc,
+  where,
 } from "firebase/firestore";
 import { mapActions, mapState } from "pinia";
 import SongUploaded from "@/components/SongUploaded.vue";
@@ -147,21 +148,19 @@ export default {
     };
   },
   async created() {
-    // Get list of user songs
-    const songsQuery = query(
-      collection(db, "songs"),
-      where("userId", "==", this.userId)
-    );
-    const songsSnapshot = await getDocs(songsQuery);
-    songsSnapshot.forEach(this.addSong);
-
-    // Get list of user albums
-    const albumsQuery = query(
-      collection(db, "albums"),
-      where("userId", "==", this.userId)
-    );
-    const albumsSnapshot = await getDocs(albumsQuery);
-    albumsSnapshot.forEach(this.addAlbum);
+    await this.getUserData();
+  },
+  beforeRouteLeave(to, from, next) {
+    !this.unsavedFlag
+      ? next()
+      : this.setNotification(
+          "notice",
+          "Unsaved changes",
+          "Save or discard changes to song details"
+        );
+  },
+  beforeUnmount() {
+    this.cancelUpload();
   },
   computed: {
     ...mapState(useUserStore, ["userId"]),
@@ -170,180 +169,96 @@ export default {
     ...mapActions(useNotificationsStore, ["setNotification"]),
 
     addSong(doc) {
-      const song = {
+      this.songs.push({
         ...doc.data(),
         id: doc.id,
-      };
-      this.songs.push(song);
+      });
     },
 
     addAlbum(doc) {
-      const album = {
+      this.albums.push({
         ...doc.data(),
         id: doc.id,
-      };
-      this.albums.push(album);
+      });
     },
 
-    async upload($event) {
-      this.isDragover = false;
-
-      const files = $event.dataTransfer
-        ? [...$event.dataTransfer.files]
-        : [...$event.target.files];
-
-      for (let i = 0; i < files.length; i++) {
-        const file = files[i];
-        if (file.type !== "audio/mpeg") return;
-
-        // Get file metadata
-        const metadata = await this.getMetadata(file);
-
-        // Create song ref
-        const songRef = ref(storage, `songs/${this.userId}/${file.name}`);
-
-        //Upload song to Storage
-        const task = uploadBytesResumable(songRef, file, metadata);
-
-        const uploadIndex =
-          this.uploads.push({
-            artist: metadata.artist,
-            current_progress: 0,
-            picture: metadata.picture,
-            task,
-            title: metadata.title,
-            variant: "",
-          }) - 1;
-
-        task.on(
-          "state_changed",
-          (snapshot) => {
-            const progress = Math.ceil(
-              (snapshot.bytesTransferred / snapshot.totalBytes) * 100
-            );
-            this.uploads[uploadIndex].current_progress = progress;
-          },
-          () => {
-            this.uploads[uploadIndex].variant = "error";
-            this.setNotification(
-              "error",
-              "Something went wrong",
-              "We couldn't upload your files"
-            );
-          },
-          async () => {
-            const song = {
-              ...metadata,
-              artistId: await this.getDocId("artists", metadata.artist),
-              fileName: file.name,
-              genreId: await this.getDocId("genres", metadata.genre),
-              userId: this.userId,
-              url: await getDownloadURL(task.snapshot.ref),
-            };
-            const songRef = await addDoc(collection(db, "songs"), song);
-            const songSnapshot = await getDoc(songRef);
-
-            this.addSong(songSnapshot);
-            this.updateAlbumDoc(metadata, songSnapshot);
-
-            this.setNotification(
-              "success",
-              "Success!",
-              "Your files just landed on the server"
-            );
-          }
-        );
-      }
-    },
-
-    async getDocId(collectionName, name) {
+    async getDocName(collectionName, name) {
       const q = query(
         collection(db, collectionName),
-        where("name", "==", name)
+        where("name", "==", name),
+        limit(1)
       );
 
       const querySnapshot = await getDocs(q);
-      const doc = querySnapshot.docs[0];
+      let docRef = querySnapshot.docs[0];
 
-      if (doc) {
-        // If doc with name exist return doc id
-        return doc.id;
-      } else {
-        // Id doesn't exist create doc and return id
-        const docRef = await addDoc(collection(db, collectionName), {
+      // If doc doesn't exist create it and return id
+      if (!doc) {
+        docRef = await addDoc(collection(db, collectionName), {
           name: name,
         });
-        return docRef;
       }
+
+      return docRef.data().name;
     },
 
-    async updateAlbumDoc(metadata, songSnapshot) {
-      const albumsQuery = query(
-        collection(db, "albums"),
-        where("artist", "==", metadata.artist),
-        where("name", "==", metadata.album)
-      );
+    async getUserData() {
+      // Query albums and songs collections and get only this user items
+      const [albumsSnapshot, songsSnapshot] = await Promise.all([
+        getDocs(
+          query(collection(db, "albums"), where("userId", "==", this.userId))
+        ),
+        getDocs(
+          query(collection(db, "songs"), where("userId", "==", this.userId))
+        ),
+      ]);
 
-      const albumsSnapshot = await getDocs(albumsQuery);
-      let albumDoc = albumsSnapshot.docs[0];
-
-      if (albumDoc) {
-        // If album with artist and name exist add new song
-        await updateDoc(doc(db, "albums", albumDoc.id), {
-          songs: arrayUnion({ id: songSnapshot.id }),
-        });
-      } else {
-        // If doesn't exist create new document
-        albumDoc = await addDoc(collection(db, "albums"), {
-          artist: metadata.artist,
-          name: metadata.album,
-          picture: metadata.picture,
-          songs: [{ id: songSnapshot.id }],
-          userId: this.userId,
-        });
-        this.addAlbum(await getDoc(albumDoc));
-      }
-
-      // Add albumId to uploaded song
-      await updateDoc(doc(db, "songs", songSnapshot.id), {
-        albumId: albumDoc.id,
-      });
+      albumsSnapshot.forEach(this.addAlbum);
+      songsSnapshot.forEach(this.addSong);
     },
 
     async getMetadata(file) {
       return new Promise((resolve, reject) => {
         jsmediatags.read(file, {
-          onSuccess: (tag) => {
+          onSuccess: async (tag) => {
             const metadata = tag.tags;
 
-            // Convert image data to JPEG file
-            const pictureBase64 = metadata.picture.data
-              .map((charCode) => String.fromCharCode(charCode))
-              .join("");
-            const pictureData = `data:${
-              metadata.picture.format
-            };base64,${window.btoa(pictureBase64)}`;
+            let pictureData = "";
+            const picture = metadata.picture;
+            if (picture) {
+              // Convert image data to JPEG file
+              const pictureBase64 = picture.data
+                .map((charCode) => String.fromCharCode(charCode))
+                .join("");
+
+              pictureData = `data:${picture.format};base64,${window.btoa(
+                pictureBase64
+              )}`;
+            }
 
             // Convert TPOS to disc info
-            const tposData = metadata.TPOS.data.split("/");
+            const tposData = metadata.TPOS
+              ? metadata.TPOS.data.split("/")
+              : ["", ""];
 
             // Convert track to track info
-            const trackData = metadata.track.split("/");
+            const trackData = metadata.track ? metadata.track.split("/") : "";
 
             // If year is store in TDRC, get only year
             // else get just year
             const yearData = metadata.TDRC
               ? metadata.TDRC.data.slice(0, 4)
-              : metadata.year;
+              : metadata.year
+              ? metadata.year
+              : "";
 
             resolve({
-              album: metadata.album,
-              artist: metadata.artist,
-              author: metadata.TCOM.data,
+              album: metadata.album || "",
+              artist: await this.getDocName("artists", metadata.artist),
+              author: metadata.TCOM.data || "",
               disc: tposData[0],
               discTotal: tposData[1],
-              format: metadata.picture.format,
-              genre: metadata.genre,
+              genre: await this.getDocName("genres", metadata.genre),
               lyrics: "",
               picture: pictureData,
               title: metadata.title,
@@ -363,6 +278,41 @@ export default {
       this.uploads.forEach((upload) => upload.task.cancel());
     },
 
+    async updateAlbumDoc(metadata, songSnapshot) {
+      const albumsQuery = query(
+        collection(db, "albums"),
+        where("artist", "==", metadata.artist),
+        where("name", "==", metadata.album),
+        limit(1)
+      );
+
+      const albumsSnapshot = await getDocs(albumsQuery);
+      let albumDoc = albumsSnapshot.docs[0];
+
+      if (albumDoc) {
+        // If album with artist and name exist add new song
+        await updateDoc(doc(db, "albums", albumDoc.id), {
+          songs: arrayUnion({ id: songSnapshot.id }),
+        });
+      } else {
+        // If doesn't exist create new document
+        albumDoc = await addDoc(collection(db, "albums"), {
+          artist: metadata.artist,
+          name: metadata.album,
+          picture: metadata.picture,
+          songs: [{ id: songSnapshot.id }],
+          userId: this.userId,
+        });
+
+        this.addAlbum(await getDoc(albumDoc));
+      }
+
+      // Add albumId to uploaded song
+      await updateDoc(doc(db, "songs", songSnapshot.id), {
+        albumId: albumDoc.id,
+      });
+    },
+
     updateAlbumPicture(albumId, picture) {
       const index = this.albums.findIndex((album) => album.id === albumId);
       this.albums[index].picture = picture;
@@ -374,7 +324,6 @@ export default {
       this.songs[index].author = values.author;
       this.songs[index].disc = values.disc;
       this.songs[index].discTotal = values.discTotal;
-      this.songs[index].format = values.format;
       this.songs[index].genre = values.genre;
       this.songs[index].lyrics = values.lyrics;
       this.songs[index].title = values.title;
@@ -401,21 +350,84 @@ export default {
       this.songs.splice(index, 1);
     },
 
+    updateProgressBar(snapshot, uploadIndex) {
+      const progress = Math.ceil(
+        (snapshot.bytesTransferred / snapshot.totalBytes) * 100
+      );
+      return (this.uploads[uploadIndex].current_progress = progress);
+    },
+
     updateUnsavedFlag(value) {
       this.unsavedFlag = value;
     },
-  },
-  beforeUnmount() {
-    this.cancelUpload();
-  },
-  beforeRouteLeave(to, from, next) {
-    !this.unsavedFlag
-      ? next()
-      : this.setNotification(
-          "notice",
-          "Unsaved changes",
-          "Save or discard changes to song details"
+
+    async upload($event) {
+      this.isDragover = false;
+
+      // Use destructuring assignment to extract files from the event object
+      const files = [...($event.dataTransfer?.files || $event.target.files)];
+
+      for (const file of files) {
+        if (file.type !== "audio/mpeg") {
+          this.setNotification(
+            "error",
+            "Wrong file format",
+            "You can only upload mpeg files"
+          );
+          return;
+        }
+
+        // Get file metadata
+        const metadata = await this.getMetadata(file);
+
+        // Create song ref
+        const songRef = ref(storage, `songs/${this.userId}/${file.name}`);
+
+        //Upload song to Storage
+        const task = uploadBytesResumable(songRef, file, metadata);
+
+        const uploadIndex =
+          this.uploads.push({
+            artist: metadata.artist,
+            current_progress: 0,
+            picture: metadata.picture,
+            task,
+            title: metadata.title,
+            variant: "",
+          }) - 1;
+
+        task.on(
+          "state_changed",
+          (snapshot) => this.updateProgressBar(snapshot, uploadIndex),
+          () => {
+            this.uploads[uploadIndex].variant = "error";
+            this.setNotification(
+              "error",
+              "Something went wrong",
+              "We couldn't upload your files"
+            );
+          },
+          async () => {
+            const songRef = await addDoc(collection(db, "songs"), {
+              ...metadata,
+              fileName: file.name,
+              userId: this.userId,
+              url: await getDownloadURL(task.snapshot.ref),
+            });
+            const songSnapshot = await getDoc(songRef);
+
+            this.addSong(songSnapshot);
+            this.updateAlbumDoc(metadata, songSnapshot);
+
+            this.setNotification(
+              "success",
+              "Success!",
+              "Your files just landed on the server"
+            );
+          }
         );
+      }
+    },
   },
 };
 </script>
