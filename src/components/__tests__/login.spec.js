d02ba73170a84218f11ca5aadcc20ee684a2f5c9
bronisMateusz <mateusz.bronis99@gmail.com>
2023-01-22 15:39:02 +0100

    Add another login test

--- src/components/__tests__/login.spec.js
+++ src/components/__tests__/login.spec.js
@@ -7,7 +7,7 @@ describe("stores", () => {
     setActivePinia(createPinia());
   });
 
-  test("authenticates user", async () => {
+  test("authenticate user with correct values", async () => {
     const store = useUserStore();
 
     expect(store.userLoggedIn).not.toBe(true);
@@ -16,5 +16,20 @@ describe("stores", () => {
       password: "SubZero278",
     });
     expect(store.userLoggedIn).toBe(true);
+  });
+
+  test("authenticate user with incorrect values", async () => {
+    const store = useUserStore();
+
+    expect(store.userLoggedIn).not.toBe(true);
+    try {
+      await store.login({
+        email: "mateusz.bronis99@gmail.com",
+        password: "SubZero278",
+      });
+    } catch (error) {
+      console.log(error);
+    }
+    expect(store.userLoggedIn).not.toBe(true);
   });
 });
