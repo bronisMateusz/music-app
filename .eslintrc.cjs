64352c47078e75f28d728beae92e05007c20659c
bronisMateusz <mateusz.bronis99@gmail.com>
2023-06-20 23:51:08 +0200

    Update .eslintrc.cjs

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,36 +1,20 @@
-import { readonly } from "vue";
-
 /* eslint-env node */
-require("@rushstack/eslint-patch/modern-module-resolution");
+require('@rushstack/eslint-patch/modern-module-resolution')
 
 module.exports = {
   root: true,
   extends: [
-    "plugin:vue/vue3-essential",
-    "eslint:recommended",
-    "@vue/eslint-config-prettier",
+    'plugin:vue/vue3-essential',
+    'eslint:recommended',
+    '@vue/eslint-config-prettier/skip-formatting'
   ],
   overrides: [
     {
-      files: ["cypress/e2e/**/*.{cy,spec}.{js,ts,jsx,tsx}"],
-      extends: ["plugin:cypress/recommended"],
-    },
-    {
-      files: ["src/components/__tests__/**.spec.js"],
-      globals: {
-        test: readonly,
-        describe: readonly,
-        expect: readonly,
-        vi: readonly,
-        beforeEach: readonly,
-        it: readonly,
-      },
-    },
+      files: ['cypress/e2e/**/*.{cy,spec}.{js,ts,jsx,tsx}'],
+      extends: ['plugin:cypress/recommended']
+    }
   ],
   parserOptions: {
-    ecmaVersion: "latest",
-  },
-  rules: {
-    "vue/multi-word-component-names": "off",
-  },
-};
+    ecmaVersion: 'latest'
+  }
+}
