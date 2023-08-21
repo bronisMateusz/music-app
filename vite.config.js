c456c02da124af779713138030c81f22d0c8347d
bronisMateusz <mateusz.bronis99@gmail.com>
2023-08-21 09:25:08 +0200

    Fix fonts import

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,7 @@
-import { fileURLToPath, URL } from "node:url";
+import { fileURLToPath, URL } from 'node:url'
 
-import { defineConfig } from "vite";
-import vue from "@vitejs/plugin-vue";
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -9,18 +9,19 @@ export default defineConfig({
     preprocessorOptions: {
       scss: {
         additionalData: `
+          @use "@/assets/scss/fonts.scss" as *;
           @use "@/assets/scss/mixins.scss" as *;
-          @use "@/assets/scss/variables.scss" as *;`,
-      },
-    },
+          @use "@/assets/scss/variables.scss" as *;`
+      }
+    }
   },
   plugins: [vue()],
   resolve: {
     alias: {
-      "@": fileURLToPath(new URL("./src", import.meta.url)),
-    },
+      '@': fileURLToPath(new URL('./src', import.meta.url))
+    }
   },
   test: {
-    global: true,
-  },
-});
+    global: true
+  }
+})
